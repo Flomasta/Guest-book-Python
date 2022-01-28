9ad39685f6cd0960ec3f956d360bfa42f1f5f402
flomasta <zatevi@mail.ru>
2022-01-28 22:50:14 +0300

    read_messages vol.1

--- cgi_bin/gbook.py
+++ cgi_bin/gbook.py
@@ -1,4 +1,6 @@
 import json
+from datetime import datetime
+import time
 
 
 class Gbook:
@@ -8,7 +10,8 @@ class Gbook:
     LOGIN_FORM_TEMPLATE = PATH + "\loginform.tmp"
     FORM_TEMPLATE = PATH + r"\form.tmp"
     CONTENT_TEMPLATE = PATH + "\content.tmp"
-    MESSAGE_TEMPLATE = PATH + "\message"
+    MESSAGE_TEMPLATE = PATH + "\message.tmp"
+    NO_RECORDS_TEMPLATE = PATH + r"\no_messages_record.tmp"
     DB = PATH + "\messages.json"
 
     def __init__(self):
@@ -16,12 +19,15 @@ class Gbook:
         self.content = ''
         self.form = ''
         self.login_form = ''
+        self.no_records = ''
 
         # присваиваем шаблоны элементов сайта переменным
         with open(self.CONTENT_TEMPLATE, 'r', encoding='utf-8') as f:
             self.content = f.read()
         with open(self.FORM_TEMPLATE, 'r', encoding='utf-8') as f:
             self.form = f.read()
+        with open(self.NO_RECORDS_TEMPLATE, 'r', encoding='utf-8') as f:
+            self.no_records = f.read()
         with open(self.LOGIN_FORM_TEMPLATE, 'r', encoding='utf-8') as f:
             self.login_form = f.read()
         try:
@@ -32,16 +38,33 @@ class Gbook:
                 json.dump({}, f)
 
     def read_message(self):
-        pass
+        with open(self.DB, 'r', encoding='utf-8') as f:
+            message = json.load(f)
+        if not message:
+            self.message = self.no_records
+            return
+        with open(self.MESSAGE_TEMPLATE, 'r', encoding='utf-8') as f:
+            message_template = f.read()
+            for id in sorted(message.keys(), reverse=True):
+                date = datetime.fromtimestamp(int(id)).strftime('%d-%M-%Y %H:%m:%S')
+                self.message += message_template.format(datetime=date, msg=message[id]['msg'],email=message[id]['email'], name=message[id]['name'])
 
     def save_message(self, name, email, message):
-        pass
+        dt = int(time.time())
+        with open(self.DB, 'r', encoding='utf-8') as f:
+            m = json.load(f)
+        m[dt] = {"name": name, "email": email, "msg": message}
+        with open(self.DB, 'w', encoding='utf-8') as f:
+            json.dump(m, f)
 
-    def delete_message(self, id):
-        pass
 
-    def login(self, login, password):
-        pass
+def delete_message(self, id):
+    pass
 
-    def logout(self):
-        pass
+
+def login(self, login, password):
+    pass
+
+
+def logout(self):
+    pass
--- cgi_bin/index.py
+++ cgi_bin/index.py
@@ -1,8 +1,12 @@
 #!C:\Users\floma\AppData\Local\Programs\Python\Python310\python.exe
+import html
 from http import cookies
 import cgitb, json
 from gbook import Gbook
 from os import environ
+from cgi import FieldStorage
+
+
 # отладчик
 cgitb.enable()
 
@@ -14,11 +18,24 @@ cookie['name']['httponly'] = 1
 print(cookie.output())
 print('Content-Type: text/html; charset =  utf-8\n')
 
-
 gbook = Gbook()
+
+
+parameters = FieldStorage()
+
+if environ.get('REQUEST_METHOD') == 'POST':
+    if parameters.getvalue('operation') == 'message':
+        name = html.escape(parameters.getvalue('name'))
+        email = html.escape(parameters.getvalue('email'))
+        msg = html.escape(parameters.getvalue('msg'))
+        gbook.save_message(name, email, msg)
+    elif parameters.getvalue('operation') == 'login':
+        login = html.escape(parameters.getvalue('login'))
+        password = html.escape(parameters.getvalue('password'))
+    elif parameters.getvalue('operation') == 'logout':
+        pass
+gbook.read_message()
 print(gbook.content.format(login_form=gbook.login_form, form=gbook.form, messages=gbook.message))
-
-
 # MY_FILE = r"..\..\gbook\templates\messages.json"
 # get для словаря  в  случае  отсутствия элемента  вернёт None, а не exception с ошибой KeyError
 # getting_cookie = environ.get('HTTP_COOKIE')
